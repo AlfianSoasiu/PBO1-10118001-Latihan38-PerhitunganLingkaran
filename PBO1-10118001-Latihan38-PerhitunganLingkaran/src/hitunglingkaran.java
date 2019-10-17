8e7b15425fcb69e40e425090e97e519c6357c02a
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-10-17 22:08:07 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan38-PerhitunganLingkaran/src/hitunglingkaran.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Memanggil subclass perhitungan lingkaran
+ */
+public class hitunglingkaran{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
